52ef823d17196b334eb1d095ce269458cc7736dc
BUDI MUKESH KUMAR <mukeshbudi18@gmail.com>
2026-08-03 17:40:34 +0530

    [1406] [Time Beats: 65.34%] [Memory Beats: 60.23%] - LeetPush

---
+++ DCP-08-26/1406-stone-game-iii/1406-Stone-Game-III.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String stoneGameIII(int[] stoneValue) {
+
+        int n = stoneValue.length;
+        int[] dp = new int[n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            dp[i] = Integer.MIN_VALUE;
+            int sum = 0;
+
+            for (int t = 1; t <= 3 && i + t <= n; t++) {
+
+                sum += stoneValue[i + t - 1];
+
+                dp[i] = Math.max(dp[i], sum - dp[i + t]);
+            }
+        }
+
+        if (dp[0] > 0) return "Alice";
+        if (dp[0] < 0) return "Bob";
+        return "Tie";
+    }
+}
