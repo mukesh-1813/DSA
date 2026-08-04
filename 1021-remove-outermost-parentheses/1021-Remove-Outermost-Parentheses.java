59d7611fcbde4cac962830d19a3ff29deb1b4f69
BUDI MUKESH KUMAR <mukeshbudi18@gmail.com>
2026-08-04 21:22:01 +0530

    [1021] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ 1021-remove-outermost-parentheses/1021-Remove-Outermost-Parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        int cnt=0;
+        StringBuilder sb = new StringBuilder();
+        for(char ch : s.toCharArray()){
+            if(ch =='('){
+                if(cnt >0){
+                    sb.append(ch);
+                }
+                   cnt++;
+            }else {
+                  cnt--;
+                if(cnt >0){
+                    sb.append(ch);
+                  
+                }
+            }
+        }
+        return sb.toString();
+    }
+}  
