bcc263915dee6f183d52378af570c481688a7e30
BUDI MUKESH KUMAR <mukeshbudi18@gmail.com>
2026-08-15 08:45:18 +0530

    [3702] [Time Beats: 68.38%] [Memory Beats: 52.21%] - LeetPush

---
+++ DCP-08-26/3702-longest-subsequence-with-non-zero-bitwise-xor/3702-Longest-Subsequence-With-Non-Zero-Bitwise-XOR.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int longestSubsequence(int[] nums) {
+        int xor = 0;
+        boolean hasNonZero = false;
+        for(int i:nums){
+            xor ^=i;
+            if(i !=0) hasNonZero = true;
+        }
+        if(xor !=0)return nums.length;
+        if(hasNonZero){
+            return nums.length-1;
+        }
+        return 0;
+
+    }
+}
